2cf24f58f8ebd5d395cee2ef4ca9d29e1e4e5a9b
June Rhodes <jrhodes@redpointgames.com.au>
2016-07-17 16:47:41 +1000

    Fix bug with Swagger SDK packaging

--- module/swagger.js
+++ module/swagger.js
@@ -42,107 +42,104 @@ function updateServer(callback) {
 function logSdk(step, sdk) {
   console.log(step + " - " + sdk);
 }
+            
+function packSdk(builders, sdk, sdkConfig, callback) {
+  if (builders[sdk] && builders[sdk].pack) {
+    logSdk('packaging', sdk);
+    builders[sdk].pack('sdks/' + sdk, 'sdks/_package/' + sdk, sdkConfig, (err) => {
+      if (!err) {
+        logSdk('packaged', sdk);
+        callback();
+      } else {
+        logSdk('failed to package', sdk);
+        callback(err);
+      }
+    });
+  } else {
+    callback();
+  }
+}
 
-function generateSdk(file, callback) {
+function buildSdk(builders, sdk, sdkConfig, callback) {
+  if (builders[sdk] && builders[sdk].build) {
+    logSdk('building', sdk);
+    builders[sdk].build('sdks/' + sdk, sdkConfig, (err) => {
+      if (!err) {
+        logSdk('built', sdk);
+        packSdk(builders, sdk, sdkConfig, callback);
+      } else {
+        logSdk('failed to build', sdk);
+        callback(err);
+      }
+    });
+  } else {
+    packSdk(builders, sdk, sdkConfig, callback);
+  }
+}
+
+function generateSdk(builders, sdk, sdkConfig, callback) {
+  var process = spawn(
+    'java',
+    [
+      '-jar',
+      'swagger-codegen-cli.jar',
+      'generate',
+      '-i',
+      'swagger.yaml',
+      '-l',
+      sdk,
+      '-o',
+      ('sdks/'+sdk),
+      '-c',
+      ('sdks/'+sdk+'.json')
+    ]
+  );
+  process.on('exit', (code) => {
+    if (code != 0) { 
+      logSdk('failed to generate', sdk);
+      callback(new Error('sdk generation exited with non-zero exit code'));
+    } else {
+      logSdk('generated', sdk);
+      buildSdk(builders, sdk, sdkConfig, callback);
+    }
+  });
+}
+
+function pregenerateSdk(file, callback) {
   var sdk = file.substring(0, file.length - 5);
   var filename = 'sdks/'+file;
-  if (filename.endsWith('.json')) {
-    logSdk('detected', sdk);
+  if (!filename.endsWith('.json')) {
+    callback();
+    return;
+  }
 
-    fs.readFile(filename, (err, data) => {
+  logSdk('detected', sdk);
+
+  fs.readFile(filename, (err, data) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    var sdkConfig = JSON.parse(data);
+
+    rimraf('sdks/'+sdk, {}, function(err) {
       if (err) {
-        callback(err);
+        callback(new Error('unable to cleanup existing package directory'));
         return;
       }
-
-      var sdkConfig = JSON.parse(data);
-
-      rimraf('sdks/'+sdk, {}, function(err) {
-        if (err) {
-          callback(new Error('unable to cleanup existing package directory'));
-          return;
-        }
-        
-        logSdk('generating', sdk);
-        var process = spawn(
-          'java',
-          [
-            '-jar',
-            'swagger-codegen-cli.jar',
-            'generate',
-            '-i',
-            'swagger.yaml',
-            '-l',
-            sdk,
-            '-o',
-            ('sdks/'+sdk),
-            '-c',
-            ('sdks/'+sdk+'.json')
-          ]
-        );
-        process.on('exit', (code) => {
-          if (code != 0) { 
-            logSdk('failed to generate', sdk);
-            callback(new Error('sdk generation exited with non-zero exit code'));
-          } else {
-            logSdk('generated', sdk);
-            
-            var doPack = function(builders, sdk, callback) {
-              if (builders[sdk] && builders[sdk].pack) {
-                logSdk('packaging', sdk);
-                builders[sdk].pack('sdks/' + sdk, 'sdks/_package/' + sdk, sdkConfig, (err) => {
-                  if (!err) {
-                    logSdk('packaged', sdk);
-                    if (builders[sdk].publishExternal) {
-                      logSdk('publishing externally', sdk);
-                      builders[sdk].publishExternal('sdks/' + sdk, 'sdks/_package/' + sdk, sdkConfig, (err) => {
-                        if (err) {
-                          logSdk('failed to publish externally', sdk);
-                        } else {
-                          logSdk('published externally', sdk);
-                        }
-
-                        callback(err);
-                      });
-                    } else {
-                      callback();
-                    }
-                  } else {
-                    logSdk('failed to package', sdk);
-                    callback(err);
-                  }
-                });
-              } else {
-                callback();
-              }
-            }
-
-            if (builders[sdk] && builders[sdk].build) {
-              logSdk('building', sdk);
-              builders[sdk].build('sdks/' + sdk, sdkConfig, (err) => {
-                if (!err) {
-                  logSdk('built', sdk);
-                  doPack(builders, sdk, callback);
-                } else {
-                  logSdk('failed to build', sdk);
-                  callback(err);
-                }
-              });
-            } else {
-              doPack(builders, sdk, callback);
-            }
-          }
-        });
-      });
+      
+      logSdk('generating', sdk);
+      generateSdk(builders, sdk, sdkConfig, callback);   
     });
-  }
+  });
 }
 
 function generateAllSdks(callback) {
   rimraf('sdks/_package', {}, function(){
     fs.mkdirSync('sdks/_package');
     var files = readdir('sdks', null, function(err, files) {
-      async.each(files, generateSdk, callback);
+      async.each(files, pregenerateSdk, callback);
     });
   });
 }
