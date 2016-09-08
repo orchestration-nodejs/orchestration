7c7dfdc4001fbe8f8d9c8c132203108779d1f968
June Rhodes <jrhodes@redpointgames.com.au>
2016-09-08 10:23:36 +1000

    Use orchestration-node from new repository

--- module/node.js
+++ module/node.js
@@ -1,18 +1,1 @@
-var spawn = require('child_process').spawn;
-
-function test(callback) {
-  var child = spawn('node', ['index.js'], {
-    env: {
-      'NODE_ENV': 'development',
-    },
-    shell: true,
-    detached: true
-  });
-  child.on('exit', (code) => {
-    callback();
-  });
-}
-
-module.exports = {
-  test: test
-};
+module.exports = require('orchestration-node');
